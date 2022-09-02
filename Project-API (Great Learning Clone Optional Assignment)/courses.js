79840bcca11e596c6cc59e0ee943b9657239adf5
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-09-03 00:15:14 +0530

    Project API-Masai

---
+++ Project-API (Great Learning Clone Optional Assignment)/courses.js
@@ -0,0 +1,115 @@
+const addCourses = async ()=>{
+    let courseObj = {
+        imageUrl : document.getElementById("courseImage").value,
+        name : document.getElementById("courseName").value,
+        price : document.getElementById("coursePrice").value,
+        desc : document.getElementById("courseDesc").value, 
+    }
+
+    let res = await fetch("https://great-learning-masai.herokuapp.com/courses",{
+        method : "POST",
+        body : JSON.stringify(courseObj),
+        headers : {
+            'Content-Type': 'application/json'
+        }
+    });
+    alert("Course added successfully");
+    location.reload();
+}
+document.getElementById("addCourseBtn").addEventListener('click', addCourses);
+
+let userId = localStorage.getItem("loggedInUserId");
+
+const displayCourses = async ()=>{
+    // document.getElementById("displayCourses").innerHTML = ""
+    let coursesRes = await fetch("https://great-learning-masai.herokuapp.com/courses");
+    let courses = await coursesRes.json();
+    // console.log(courses);
+    courses.map(async (element,index)=>{
+
+        let div = document.createElement('div');
+
+        let image = document.createElement('img');
+        image.setAttribute('src', element.imageUrl);
+        image.addEventListener('click', ()=>{
+            goToDescription(element.id);
+            
+        });
+        let title = document.createElement('h2');
+        title.innerText = element.name;
+
+        let price = document.createElement('h4');
+        price.innerText = "Price : "+element.price;
+
+        let description = document.createElement('p');
+        description.innerText = "Desc : "+element.desc;
+
+        let wishlist = document.createElement('button');
+        wishlist.innerText = "Add to Wishlist";
+        wishlist.setAttribute("id","addToWishlist")
+        wishlist.addEventListener('click', () =>{
+            addToWishlist(element);
+        })
+
+        let userRes = await fetch(`https://great-learning-masai.herokuapp.com/users/${userId}`);
+        let userData = await userRes.json();
+        let userRole = userData.role;
+
+        if(userRole == "student" || userRole == "admin"){
+            div.append(image,title,price,description,wishlist);
+            document.getElementById("addCourse").style.display = "none";
+            document.getElementById("addCour").style.display = "none";
+        }
+        else{
+            let remove = document.createElement('button');
+            remove.innerText = "Remove";
+            remove.setAttribute("id","remCourse")
+            remove.addEventListener('click', () =>{
+                removed(element,index);
+            })
+
+            div.append(image,title,price,description,wishlist,remove);
+        }
+
+        document.getElementById("displayCourses").append(div);
+    });
+}
+displayCourses();
+
+const removed = async (element,index)=>{
+    // console.log(index);
+    let id = element.id; // element is
+    // console.log(id);
+    let removeCourseRes = await fetch(`https://great-learning-masai.herokuapp.com/courses/${id}`,{
+        method : "DELETE",
+    });
+    alert("Course removed")
+    location.reload(); 
+}
+
+// let userId = localStorage.getItem("loggedInUserId");
+const addToWishlist = async (element)=>{
+    let wishlistObj = {
+            imageUrl: element.imageUrl,
+            name: element.name,
+            price: element.price,
+            desc: element.desc,
+    };
+    let addToWishlist = await fetch(`https://great-learning-masai.herokuapp.com/users/${userId}/wishlist`,{
+        method : "POST",
+        body : JSON.stringify(wishlistObj),
+        headers : {
+            'Content-Type': 'application/json'
+        }
+    });    
+    alert("Course added to the wishlist")
+    location.reload();                 
+}
+
+const goToDescription =(courseId) => {
+    localStorage.setItem("courseId",courseId);
+    console.log(courseId);
+    window.location.href = "./courseDescription.html";
+}
+
+
---
+++ Project-API (Great Learning Clone Optional Assignment)/index.js
@@ -0,0 +1,112 @@
+const logout = async()=>{
+    let res = await fetch("https://great-learning-masai.herokuapp.com/loggedInUser");
+    let data = await res.json();
+    // data.filter((element)=>{
+
+    // })
+    // Remember ==> i have to map and find the user by using email then get id and then delete that particular user
+    let id = localStorage.getItem("loggedInUserId");
+
+    let deleteRes = await fetch(`https://great-learning-masai.herokuapp.com/loggedInUser/${id}`,{
+        method: "DELETE",
+    });
+    window.location.href = "./index.html";
+    // console.log(deleteRes);
+    localStorage.removeItem("loggedInUserId");
+    // localStorage.removeItem("courseId");
+}
+
+// Functionalities Section
+
+var userId = localStorage.getItem("loggedInUserId") || 0;
+
+const displayUser = async ()=>{
+
+    if(userId>0){
+        document.getElementById("buttons").innerHTML = "";
+        let user = await fetch(`https://great-learning-masai.herokuapp.com/users/${userId}`);
+        let userData = await user.json();
+        let name = "Hello, " +userData.name;
+        
+        let anchor = document.createElement("a");
+        anchor.append(name);
+        anchor.setAttribute("href", "./profile.html");
+        document.getElementById("buttons").append(anchor);
+    }
+
+}
+displayUser();
+
+const displayCourse = async ()=>{
+    // document.getElementById("displayCourses").innerHTML = ""
+    let coursesRes = await fetch("https://great-learning-masai.herokuapp.com/courses");
+    let courses = await coursesRes.json();
+    // console.log(courses);
+    courses.map(async (element,index)=>{
+        let div = document.createElement('div');
+
+        let image = document.createElement('img');
+        image.setAttribute('src', element.imageUrl);
+        image.addEventListener('click', ()=>{
+            if(userId ===0){
+                window.location.href = "./login.html";
+            }
+            else{
+                window.location.href = "./courses.html";
+            }
+        });
+        let title = document.createElement('h2');
+        title.innerText = element.name;
+
+        let price = document.createElement('h4');
+        price.innerText = "Price : "+element.price;
+
+        let description = document.createElement('p');
+        description.innerText = "Desc : "+element.desc;
+
+        div.append(image,title,price,description);
+        document.getElementById("displayCourse").append(div);
+    });
+}
+displayCourse();
+
+// console.log(userId);
+const displayNavbar = ()=>{
+    var indCourse = document.createElement("a");
+    indCourse.innerText = "Course";
+
+    var indSuccessStories = document.createElement("a");
+    indSuccessStories.innerText = "Success Stories";
+
+    var indBlog = document.createElement("a");
+    indBlog.innerText = "Blog";
+
+    var indCartpage = document.createElement("a");
+    indCartpage.innerText = "Cart";
+
+    var indWishlist = document.createElement("a");
+    indWishlist.innerText = "Wishlist";
+
+    if(userId === 0){
+        indCourse.setAttribute("href","./login.html");
+        indSuccessStories.setAttribute("href","./login.html");
+        indBlog.setAttribute("href","./login.html");
+        indCartpage.setAttribute("href","./login.html");
+        indWishlist.setAttribute("href","./login.html");
+    }
+    else{
+        indCourse.setAttribute("href","./courses.html");
+        indSuccessStories.setAttribute("href","https://www.mygreatlearning.com/alumni");
+        indBlog.setAttribute("href","https://www.mygreatlearning.com/blog");
+        indCartpage.setAttribute("href","./cartpage.html");
+        indWishlist.setAttribute("href","./wishlist.html");
+    }
+
+    document.getElementById("indexNavbar").append(indCourse,indSuccessStories,indBlog,indCartpage,indWishlist);
+}
+displayNavbar();
+
+
+
+
+
